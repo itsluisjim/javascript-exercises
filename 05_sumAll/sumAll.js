eecd303411ec2068c9b3bf90d01ffc80b8dbdf3c
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-17 17:58:16 -0500

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if(Array.isArray(num2) || Array.isArray(num1) || num1 < 0 || num2 < 0 || typeof num1 == "string" || typeof num2 == "string"){
+        return "ERROR";
+    }
+
+    for(let i = Math.min(num1,num2);i<=Math.max(num1,num2);i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
