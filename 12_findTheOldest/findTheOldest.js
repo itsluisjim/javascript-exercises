e471de938e504bb867ab2e7a329d271f0f8bb1c3
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-31 15:33:40 -0500

    Add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+   
+    let stillAlive = arr.filter(person => !person.hasOwnProperty('yearOfDeath'))
 
+    let year = new Date().getFullYear();
+    if(stillAlive.length == 1){
+        stillAlive[0]['yearOfDeath'] = year;
+    }
+
+    let oldToYoung = arr.sort((a,b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth? -1: 1);
+
+    return oldToYoung[0];
 };
 
 // Do not edit below this line
