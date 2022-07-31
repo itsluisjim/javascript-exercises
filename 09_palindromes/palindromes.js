7865b34ad608efb6b355dddf8e7a0e08b5cf2b65
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-30 22:01:19 -0500

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+       
+    let regex = /[a-z0-9]/;
+    
+    let arrStr = Array.from(str.toLowerCase());
+     
+    let stringAsArr = arrStr.filter(item =>  item.match(regex))
+    
+    
+    let finalFilteredStr = stringAsArr.join('');
+    let reversedFilteredStr = stringAsArr.reverse().join('');
+    
+
+    let isPalindrome = false;
+    if( finalFilteredStr == reversedFilteredStr){
+        isPalindrome = true;
+    }
+    return isPalindrome;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
